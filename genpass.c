353832cbcac55abe3578f7e3d2705da03c66cece
3T5K <ivank.dbt@gmail.com>
2025-03-16 14:24:38 +0100

    chaging names, whitespace...

--- genpass.c
+++ genpass.c
@@ -31,22 +31,21 @@
 #include <string.h>
 
 #define MIN(a, b) ((a < b) ? a : b)
-#define CHARS_UPPER "abcdefghijklmnopqrstuvwxyz"
-#define CHARS_LOWER "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-#define CHARS_DIGITS "0123456789"
-#define CHARS_SPECIAL "`~!@#$%^&*()_-+={}[]:;'\"\\|/?.>,<"
-#define CHARS_ALL CHARS_UPPER CHARS_LOWER CHARS_DIGITS CHARS_SPECIAL
-#define CHARS_NO_BIAS CHARS_UPPER CHARS_LOWER CHARS_DIGITS ":@"
-#define CHAR_AMOUNT (uint8_t)94
-#define C_DFLT 0  // 0b0000 0000
-#define C_SPEC 1  // 0b0000 0001
-#define C_UPPR 2  // 0b0000 0010
-#define C_LOWR 4  // 0b0000 0100
-#define C_DIGT 8  // 0b0000 1000
-#define C_FAIR 16 // 0b0001 0000
-#define UPPER_BOUND 100 * 1000
 #define BUF_SIZE 50 * 1000
-const char *RANDFILE   = "/dev/urandom";
+#define C_UPPR "abcdefghijklmnopqrstuvwxyz"
+#define C_LOWR "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define C_DIGT "0123456789"
+#define C_SPEC "`~!@#$%^&*()_-+={}[]:;'\"\\|/?.>,<"
+#define C_FULL C_UPPR C_LOWR C_DIGT C_SPEC
+#define C_FAIR C_UPPR C_LOWR C_DIGT ":@"
+#define C_FULL_COUNT (uint8_t)94
+#define M_DFLT 0  // 0b00000000
+#define M_SPEC 1  // 0b00000001
+#define M_UPPR 2  // 0b00000010
+#define M_LOWR 4  // 0b00000100
+#define M_DIGT 8  // 0b00001000
+#define M_FAIR 16 // 0b00010000
+const char *P_RNG      = "/dev/urandom";
 const char *const HELP =
     "This simple utility generates random passwords in ASCII armor\n"
     "using data from /dev/urandom.\n"
@@ -69,8 +68,6 @@ const char *const HELP =
     "If any of these flags (including '-f') is used, only the specified\n"
     "characters will be included in the output.\n"
     "Bias rejection is not performed. For unbiased output, use '--fair'.\n";
-    // "The argument to '--length' or '--count' has an\n"
-    // "upper bound of (size_t)-1.\n";
 
 void die(void) {
     fprintf(stderr, "try running with '--help' or '-h' for more info\n");
@@ -78,14 +75,14 @@ void die(void) {
 }
 
 size_t try_parse_num_arg() {
-    char *endptr; errno = 0;
-    const size_t val = strtoul(optarg, &endptr, 10);
+    char *end; errno = 0;
+    const size_t val = strtoul(optarg, &end, 10);
     if (errno) {
         fprintf(stderr, "argument for '--length' out of range\n");
         die();
     }
 
-    if (*endptr != '\0') {
+    if (*end != '\0') {
         fprintf(stderr, "argument for '--length' contains unexpected character(s)\n");
         die(); 
     }
@@ -94,13 +91,13 @@ size_t try_parse_num_arg() {
 }
 
 void armorize(
-    uint8_t *buf,
-    const size_t sz,
+    uint8_t           *buf,
+    const size_t      sz,
     const char *const charset,
-    const size_t cs_len
+    const size_t      c_len
 ) {
     for (size_t i = 0; i < sz; i++) {
-        buf[i] = charset[buf[i] % cs_len];
+        buf[i] = charset[buf[i] % c_len];
     }
 }
 
@@ -119,20 +116,25 @@ int main(const int argc, char *const *const argv) {
         {NULL,      0,                 NULL, 0  }
     };
 
-    size_t pw_len = 30, pw_count = 1; const char *dst_path = NULL; int opt;
-    static char charset[CHAR_AMOUNT + 1] = {0}; size_t cs_len; int cs = C_DFLT;
+    size_t      pw_len                    = 30;
+    size_t      pw_count                  = 1;
+    const char *p_dst                     = NULL;
+    static char charset[C_FULL_COUNT + 1] = {0};
+    int         c_mask                    = M_DFLT;
+    size_t      c_len;
+    int         opt;
     while ((opt = getopt_long(argc, argv, "hrn:c:o:ulsdf", opts, NULL)) != -1) {
         switch (opt) {
             case 'h': printf("%s", HELP);             return EXIT_SUCCESS;
-            case 'r': RANDFILE = "/dev/random";       break;
-            case 'o': dst_path = optarg;              break;
+            case 'r': P_RNG = "/dev/random";          break;
+            case 'o': p_dst = optarg;                 break;
             case 'n': pw_len = try_parse_num_arg();   break;
             case 'c': pw_count = try_parse_num_arg(); break;
-            case 'u': cs |= C_UPPR;                   break;
-            case 'l': cs |= C_LOWR;                   break;
-            case 's': cs |= C_SPEC;                   break;
-            case 'd': cs |= C_DIGT;                   break;
-            case 'f': cs =  C_FAIR;                   break;
+            case 'u': c_mask |= M_UPPR;               break;
+            case 'l': c_mask |= M_LOWR;               break;
+            case 's': c_mask |= M_SPEC;               break;
+            case 'd': c_mask |= M_DIGT;               break;
+            case 'f': c_mask  = M_FAIR;               break;
             default: die();
         }
     }
@@ -142,51 +144,51 @@ int main(const int argc, char *const *const argv) {
         die();
     }
 
-    if (cs == C_DFLT) {
-        strcpy(charset, CHARS_ALL);
-    } else if (cs == C_FAIR) {
-        strcpy(charset, CHARS_NO_BIAS);
+    if (c_mask == M_DFLT) {
+        strcpy(charset, C_FULL);
+    } else if (c_mask == M_FAIR) {
+        strcpy(charset, C_FAIR);
     } else {
-        if (cs & C_UPPR) { strcat(charset, CHARS_UPPER);   }
-        if (cs & C_LOWR) { strcat(charset, CHARS_LOWER);   }
-        if (cs & C_SPEC) { strcat(charset, CHARS_SPECIAL); }
-        if (cs & C_DIGT) { strcat(charset, CHARS_DIGITS);  }
+        if (c_mask & M_UPPR) { strcat(charset, C_UPPR);   }
+        if (c_mask & M_LOWR) { strcat(charset, C_LOWR);   }
+        if (c_mask & M_SPEC) { strcat(charset, C_SPEC); }
+        if (c_mask & M_DIGT) { strcat(charset, C_DIGT);  }
     }
 
-    cs_len = strlen(charset);
-    const int rand_src = open(RANDFILE, O_RDONLY);
-    if (rand_src == -1) {
-        fprintf(stderr, "failed to open %s\n", RANDFILE); 
+    c_len = strlen(charset);
+    const int fd_rng = open(P_RNG, O_RDONLY);
+    if (fd_rng == -1) {
+        fprintf(stderr, "failed to open %s\n", P_RNG); 
         return EXIT_FAILURE;
     }
 
-    const int f_out = (dst_path) ? open(dst_path, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR) : STDOUT_FILENO;
-    if (f_out == -1) {
-        fprintf(stderr, "failed to open \"%s\": ", dst_path);
+    const int fd_dst = (p_dst) ? open(p_dst, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR) : STDOUT_FILENO;
+    if (fd_dst == -1) {
+        fprintf(stderr, "failed to open \"%s\": ", p_dst);
         perror("");
         return EXIT_FAILURE;
     }
 
     static uint8_t buf[BUF_SIZE] = {0};
     for (size_t pw = 0; pw < pw_count; pw++) {
-        for (size_t printed = 0; printed != pw_len;) {
-            if (read(rand_src, buf, BUF_SIZE) != BUF_SIZE) {
-                fprintf(stderr, "failed to read %i bytes from %s\n", BUF_SIZE, RANDFILE);
+        for (size_t written = 0; written != pw_len;) {
+            if (read(fd_rng, buf, BUF_SIZE) != BUF_SIZE) {
+                fprintf(stderr, "failed to read %i bytes from %s\n", BUF_SIZE, P_RNG);
                 return EXIT_FAILURE;
             }
 
-            armorize(buf, BUF_SIZE, charset, cs_len);
-            const size_t out = write(f_out, buf, MIN(BUF_SIZE, pw_len - printed));
-            if (out != MIN(BUF_SIZE, pw_len - printed)) {
-                fprintf(stderr, "failed to write to %s\n", dst_path);
+            armorize(buf, BUF_SIZE, charset, c_len);
+            const size_t w = write(fd_dst, buf, MIN(BUF_SIZE, pw_len - written));
+            if (w != MIN(BUF_SIZE, pw_len - written)) {
+                fprintf(stderr, "failed to write to %s\n", p_dst);
                 return EXIT_FAILURE;
             }
 
-            printed += out;
+            written += w;
         }
 
-        if (write(f_out, "\n", 1) != 1) {
-            fprintf(stderr, "failed to write to %s\n", dst_path);
+        if (write(fd_dst, "\n", 1) != 1) {
+            fprintf(stderr, "failed to write to %s\n", p_dst);
             return EXIT_FAILURE;
         };
     }
